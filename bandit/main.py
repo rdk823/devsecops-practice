70df742bb22ec52e5b531d004014ea8cd2bfcf2f
Roger Dansou <roger.dansou@telus.com>
2024-10-24 12:20:31 -0400

    fix: use safe_load() instead of load() function

--- bandit/main.py
+++ bandit/main.py
@@ -5,7 +5,7 @@ import yaml
 def unsafe_yaml_load(filepath):
     with open(filepath, 'r') as file:
         # Usage loading of YAML content
-        data = yaml.load(file)
+        data = yaml.safe_load(file)
         return data
 
 
